82e6d822a782088fd55bf69e330e35a0a0494a6e
louis-moon <50563558+louis-moon@users.noreply.github.com>
2025-09-14 23:53:45 -0400

    build: switch to SSR with Netlify Next runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',   // ⚠️ tells Next.js to emit /out instead of .next
   images: { unoptimized: true },        // required for export
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
